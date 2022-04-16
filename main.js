91abe025652c91e9e6532d92bd02eda426b80c23
VanDickerson <54822865+VanDickerson@users.noreply.github.com>
2022-04-15 20:30:48 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.header').height($(window).height());
+})
+/*
+element = document.getElementByID("github");
+element.addEventListener("click", function(){ 
+   window.location.href = "https://github.com/Nessa0001";
+}); */
